8fda0b267fe37ef87f4086a6501fe5358cfe266a
MarikrmenCarrillo <58841062+MarikrmenCarrillo@users.noreply.github.com>
2023-11-25 12:14:31 -0600

    Add files via upload

---
+++ MVCReactApp/ClientApp/src/App.js
@@ -0,0 +1,51 @@
+﻿import "bootstrap/dist/css/bootstrap.min.css"
+import { useEffect, useState } from "react"
+
+
+const App = () => {
+    const [empleado, setEmpleado] = useState([])
+
+    const ConsumirAPI = async () => {
+        const response = await fetch("api/empleado/obtenerEmpleados")
+
+        if (response.ok) {
+            const data = await response.json()
+            console.log(data)
+            setEmpleado(data)
+        }
+    }
+
+    useEffect(() => {
+        ConsumirAPI()
+    })
+
+    return(
+        <div className="container-fluid">
+            <h5>Lista de empleados</h5>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Correo</th>
+                        <th>Direccion</th>
+                        <th>Telefono</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        empleado.map((empleado) => (
+                            <tr key={empleado.idEmpleado }>
+                                <td>{empleado.nombre}</td>
+                                <td>{empleado.correo}</td>
+                                <td>{empleado.direccion}</td>
+                                <td>{empleado.telefono}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default App
---
+++ MVCReactApp/ClientApp/src/components/Tarjeta.js
@@ -0,0 +1,12 @@
+﻿const Tarjeta = (props) => {
+    return (
+        <div className="card text-center bg-dark mt-5 ">
+            <div className="card-body">
+                <h1 className="card-title text-info">{ props.titulo || "Título de la tarjeta"}</h1>
+                <p className="card-text text-light">{props.descripcion || "Descripción de la tarjeta"}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Tarjeta
